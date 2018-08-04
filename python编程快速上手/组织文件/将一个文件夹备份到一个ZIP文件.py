4e2c8aa1545287857a6da147d87d2d8ac2bbd21e
A1401021150\Administrator <leaf.ye@cygia.com>
2018-08-04 12:12:52 +0800

    8/4

--- python编程快速上手/组织文件/将一个文件夹备份到一个ZIP文件.py
+++ python编程快速上手/组织文件/将一个文件夹备份到一个ZIP文件.py
@@ -10,17 +10,31 @@ def backupToZip(folder):
     folder = os.path.abspath(folder) # make sure folder is absolute
     print(folder)
     # TODO:Figure out the filename this code should use based on
-
-
-    # TODO:what files already exist.
+    number = 1
+    while True:
+        zipfileName = os.path.basename(folder) + '_' + str(number) + '.zip'
+        print(zipfileName)
+        # TODO:what files already exist.
+        if not os.path.exists(zipfileName):
+            break
+        number += 1
 
     # TODO: Create the ZIP file.
-
+    print("Creating %s ... "% zipfileName)
+    backupZip = zipfile.ZipFile(zipfileName,'w')
     # TODO: Walk the entire folder tree and compress the files in each folder.
-
-    # Add the current folder to the ZIP file.
-
-    # Add all the files in this folder to the ZIP file.
+    for folderName,subfolders,fileNames in os.walk(folder):
+        print('Adding files in %s...' % (folderName))
+        # Add the current folder to the ZIP file.
+        backupZip.write(folderName)
+        # Add all the files in this folder to the ZIP file.
+        for fileName in fileNames:
+            print('Adding files in %s...' % (fileName))
+            newBase = os.path.basename(folder) + '_'
+            if fileName.startswith(newBase) and fileName.endswith(".zip"):
+                continue
+            backupZip.write(os.path.join(folderName,fileName))
+    backupZip.close()
 
 
 backupToZip('.')
